53418a0c2774995f8ea51317b19d324f9a6349f4
Douglas Leandro <douglas.gtads@gmail.com>
2025-08-11 13:59:23 -0300

    🔧 refactor: Moving Spinner inside of Dom's location by portal

--- src/components/molecules/SpinnerCovering/index.tsx
+++ src/components/molecules/SpinnerCovering/index.tsx
@@ -1,6 +1,7 @@
 import { Backdrop } from '@/components/atoms/Backdrop';
 import { Spinner } from '@/components/atoms/Spinner';
 import { ComponentPropsWithoutRef } from 'react';
+import { createPortal } from 'react-dom';
 import styles from './SpinnerCovering.module.scss';
 
 type SpinnerCoveringProps = Omit<
@@ -8,8 +9,10 @@ type SpinnerCoveringProps = Omit<
     'children'
 >;
 
-export const SpinnerCovering = (props: SpinnerCoveringProps) => (
-    <Backdrop {...props}>
-        <Spinner className={styles.spinner} />
-    </Backdrop>
-);
+export const SpinnerCovering = (props: SpinnerCoveringProps) =>
+    createPortal(
+        <Backdrop {...props}>
+            <Spinner className={styles.spinner} />
+        </Backdrop>,
+        document.getElementById('cover') ?? document.createDocumentFragment()
+    );
