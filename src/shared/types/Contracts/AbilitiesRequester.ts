cd17c9d19dcc962915074fa60fb1a0f856a6945e
Douglas Leandro <douglas.gtads@gmail.com>
2025-08-28 21:53:39 -0300

    🗑️ remove: deprecated and unused code

--- src/shared/types/Contracts/AbilitiesRequester.ts
+++
@@ -1,8 +0,0 @@
-import { type HttpClient } from './HttpClient';
-
-export interface AbilitiesRequester {
-    /**
-     * Search the user's abilities
-     */
-    request(id: number, token: string): ReturnType<HttpClient['request']>;
-}
